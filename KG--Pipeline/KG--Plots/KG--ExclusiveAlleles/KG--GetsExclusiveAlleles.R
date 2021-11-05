ada50557dee73372f3f7b348dc106c837deee15e
g-pacheco <ganpa@aqua.dtu.dk>
2021-11-05 22:46:47 +0100

    Expanded KG--ExclusiveAlleles.

--- KG--Pipeline/KG--Plots/KG--ExclusiveAlleles/KG--GetsExclusiveAlleles.R
+++ KG--Pipeline/KG--Plots/KG--ExclusiveAlleles/KG--GetsExclusiveAlleles.R
@@ -37,6 +37,7 @@ for(ROW in 1:nrow(Geno)){
           all(Geno[ROW, c(9:10, 12:22)] == 2), "HomoFixed_Kher",
           "Other")}
 
+
 # Gets numbers for HomoFixedKher_AlleleState ~
 table(Geno$HomoFixedKher_AlleleState)
 
@@ -58,6 +59,7 @@ for(ROW in 1:nrow(HomoFixedKher_df)){
     ifelse(all(HomoFixedKher_df[ROW, c(5:8, 11)] == 0) & all(HomoFixedKher_df[ROW, c(9:10, 12:22)] == 2) | 
            all(HomoFixedKher_df[ROW, c(5:8, 11)] == 2) & all(HomoFixedKher_df[ROW, c(9:10, 12:22)] == 0), "HomoFixed_KherKspESP",
            "Other")}
+
 
 # Gets numbers for HomoFixed_KherKspESP_AlleleState ~
 table(HomoFixedKher_df$HomoFixed_KherKspESP_AlleleState)
@@ -81,6 +83,9 @@ table(HomoFixedKher_df$HomoFixed_Kher_Het_KspESP_AlleleState)
 Geno$ExclusiveToKspESP_AlleleState <- rep(NA, nrow(Geno))
 Geno$ExclusiveToKher_AlleleState <- rep(NA, nrow(Geno))
 Geno$HomoFixedKspESP_AlleleState <- rep(NA, nrow(Geno))
+Geno$HetKspESP_AlleleState <- rep(NA, nrow(Geno))
+Geno$HetKher_AlleleState <- rep(NA, nrow(Geno))
+Geno$HetKspESP_Kher_AlleleState <- rep(NA, nrow(Geno))
 
 
 ## 4) How many of the 5,688 SNPs are homozygous and fixed (either REF or ALT) across all 5 KspESP individuals?
@@ -93,6 +98,7 @@ for(ROW in 1:nrow(Geno)){
          all(Geno[ROW, c(5:8, 11)] == 2), "HomoFixed_KspESP",
          "Other")}
 
+
 # Gets numbers for HomoFixedKspESP_AlleleState ~
 table(Geno$HomoFixedKspESP_AlleleState)
 
@@ -103,8 +109,10 @@ table(Geno$HomoFixedKspESP_AlleleState)
 # Fills ExclusiveToKspESP_AlleleState ~
 for(ROW in 1:nrow(Geno)){
   Geno[ROW, "ExclusiveToKspESP_AlleleState"] <-
-   ifelse(all(Geno[ROW, c(5:8, 11)] == 0) & all(Geno[ROW, c(9:10, 12:22)] == 2), "ExclusiveToKspESP_REF",
-   ifelse(all(Geno[ROW, c(5:8, 11)] == 2) & all(Geno[ROW, c(9:10, 12:22)] == 0), "ExclusiveToKspESP_ALT",
+   ifelse(all(Geno[ROW, c(5:8, 11)] == 0) & all(Geno[ROW, c(9:10, 12:22)] == 2) |
+          all(Geno[ROW, c(5:8, 11)] == 1) & all(Geno[ROW, c(9:10, 12:22)] == 2), "ExclusiveToKspESP_REF",
+   ifelse(all(Geno[ROW, c(5:8, 11)] == 2) & all(Geno[ROW, c(9:10, 12:22)] == 0) |
+          all(Geno[ROW, c(5:8, 11)] == 1) & all(Geno[ROW, c(9:10, 12:22)] == 0), "ExclusiveToKspESP_ALT",
    "Other"))}
 
 
@@ -117,14 +125,59 @@ table(Geno$ExclusiveToKspESP_AlleleState)
 
 # Fills ExclusiveToKher_AlleleState ~
 for(ROW in 1:nrow(Geno)){
-  Geno[ROW, "ExclusiveToKher_AlleleState"] <-
-   ifelse(all(Geno[ROW, c(9:10, 12:22)] == 0) & all(Geno[ROW, c(5:8, 11)] == 2), "ExclusiveToKher_REF",
-   ifelse(all(Geno[ROW, c(9:10, 12:22)] == 2) & all(Geno[ROW, c(5:8, 11)] == 0), "ExclusiveToKher_AÆT",
+ Geno[ROW, "ExclusiveToKher_AlleleState"] <-
+   ifelse(all(Geno[ROW, c(9:10, 12:22)] == 0) & all(Geno[ROW, c(5:8, 11)] == 2) |
+          all(Geno[ROW, c(9:10, 12:22)] == 1) & all(Geno[ROW, c(5:8, 11)] == 2), "ExclusiveToKher_REF",
+   ifelse(all(Geno[ROW, c(9:10, 12:22)] == 2) & all(Geno[ROW, c(5:8, 11)] == 0) |
+          all(Geno[ROW, c(9:10, 12:22)] == 1) & all(Geno[ROW, c(5:8, 11)] == 0), "ExclusiveToKher_ALT",
    "Other"))}
 
 
-# Gets numbers for ExclusiveToKher_AlleleState ~
+# Gets numbers for ExclusiveToKspESP_AlleleState ~
 table(Geno$ExclusiveToKher_AlleleState)
+
+
+## 7) How many SNPs are heterozygous in KspESP?
+
+
+# Fills HetKspESP_AlleleState ~
+for(ROW in 1:nrow(Geno)){
+ Geno[ROW, "HetKspESP_AlleleState"] <-
+   ifelse(all(Geno[ROW, c(5:8, 11)] == 1), "HetKspESP",
+          "Other")}
+
+
+# Gets numbers for HetKspESP_AlleleState ~
+table(Geno$HetKspESP_AlleleState)
+
+
+## 8) How many SNPs are heterozygous in Kher?
+
+
+# Fills HetKher_AlleleState ~
+for(ROW in 1:nrow(Geno)){
+   Geno[ROW, "HetKher_AlleleState"] <-
+    ifelse(all(Geno[ROW, c(9:10, 12:22)] == 1), "HetKher",
+           "Other")}
+
+
+# Gets numbers for HetKher_AlleleState ~
+table(Geno$HetKher_AlleleState)
+
+
+## 9) How many SNPs are heterozygous in KspESP & Kher?
+
+
+# Fills HetKspESP_Kher_AlleleState ~
+for(ROW in 1:nrow(Geno)){
+ Geno[ROW, "HetKspESP_Kher_AlleleState"] <-
+    ifelse(all(Geno[ROW, c(5:8, 11)] == 1) &
+           all(Geno[ROW, c(9:10, 12:22)] == 1), "HetKspESP_Kher",
+           "Other")}
+
+
+# Gets numbers for HetKspESP_Kher_AlleleState ~
+table(Geno$HetKspESP_Kher_AlleleState)
 
 
 #
